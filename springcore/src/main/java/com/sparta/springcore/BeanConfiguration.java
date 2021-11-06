b67ba06282c6da05c27e99025da910e299e0e00d
profoundsea25 <profoundsea25@gmail.com>
2021-11-07 03:04:44 +0900

    Spring 심화반

--- springcore/src/main/java/com/sparta/springcore/BeanConfiguration.java
+++ springcore/src/main/java/com/sparta/springcore/BeanConfiguration.java
@@ -1,22 +1,24 @@
-package com.sparta.springcore;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class BeanConfiguration {
-    @Bean
-    public ProductRepository productRepository() {
-        String dbId = "sa";
-        String dbPassword = "";
-        String dbUrl = "jdbc:h2:mem:springcoredb";
-        return new ProductRepository(dbId, dbPassword, dbUrl);
-    }
-
-    @Bean // 객체 등록
-    @Autowired // Bean으로 선언된 객체를 의존성 주입(DI) = 만들어진 객체를 활용
-    public ProductService productService(ProductRepository productRepository) {
-        return new ProductService(productRepository);
-    }
-}
+//package com.sparta.springcore;
+//
+//import com.sparta.springcore.repository.ProductRepository;
+//import com.sparta.springcore.service.ProductService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class BeanConfiguration {
+//    @Bean
+//    public ProductRepository productRepository() {
+//        String dbId = "sa";
+//        String dbPassword = "";
+//        String dbUrl = "jdbc:h2:mem:springcoredb";
+//        return new ProductRepository(dbId, dbPassword, dbUrl);
+//    }
+//
+//    @Bean // 객체 등록
+//    @Autowired // Bean으로 선언된 객체를 의존성 주입(DI) = 만들어진 객체를 활용
+//    public ProductService productService(ProductRepository productRepository) {
+//        return new ProductService(productRepository);
+//    }
+//}
